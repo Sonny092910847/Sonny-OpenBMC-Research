d61c29319ecc8f875ff73249b22c8bb9c0c1dbfb
Sonny092910847 <jasongod5737@gmail.com>
2025-12-15 09:46:30 +0800

    Update test-temp-sensor.cpp

--- Newcomer_Training/L5/Task1_TEST_Temp_Sensor/v1.0_basic/files/test-temp-sensor.cpp
+++ Newcomer_Training/L5/Task1_TEST_Temp_Sensor/v1.0_basic/files/test-temp-sensor.cpp
@@ -180,8 +180,8 @@ int main()
             checkThresholds(temp, warningIface, criticalIface, conn); // 4. 呼叫checkThresholds檢查是否超過門檻
         }
         
-        timer.expires_after(std::chrono::seconds(1));                 // 5. 1 秒後再執行
-        timer.async_wait(readTemp);                                   // 6. 等待並呼叫自己
+        timer.expires_after(std::chrono::seconds(1));                 // 5. 重新設定 1 秒
+        timer.async_wait(readTemp);                                   // 6. 到期後(1秒後)再呼叫自己
     };
     
     timer.expires_after(std::chrono::seconds(1));
